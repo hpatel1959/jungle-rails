4a16ac5ab158a37dc36c393d03e702dc181d6d5d
Harsh Patel <hpatel1959@hotmail.com>
2023-04-07 20:16:02 +0000

    fix error with basic auth for admin product page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,7 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD'], only: [:index]
 
   def index
-    http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD'], only: [:index]
-
     @products = Product.order(id: :desc).all
   end
 
